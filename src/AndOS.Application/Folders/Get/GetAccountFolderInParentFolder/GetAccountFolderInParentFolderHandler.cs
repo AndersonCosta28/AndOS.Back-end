981ff8548b1b5e3cde3dcd4d3b98c52aa59c7b7b
Anderson da Silva Costa <anderson.scosta28@gmail.com>
2024-08-17 13:02:46 -0300

    fix: correção de loop infinito

--- src/AndOS.Application/Folders/Get/GetAccountFolderInParentFolder/GetAccountFolderInParentFolderHandler.cs
+++ src/AndOS.Application/Folders/Get/GetAccountFolderInParentFolder/GetAccountFolderInParentFolderHandler.cs
@@ -8,9 +8,10 @@ public class GetAccountFolderInParentFolderHandler(IReadRepository<Folder> folde
     public async Task<Folder> Handle(GetAccountFolderInParentFolderRequest request, CancellationToken cancellationToken)
     {
         Folder folder = null;
+        var folderId = request.ParentFolderId;
         do
         {
-            folder = await folderRepository.FirstOrDefaultAsync(new GetFolderByIdSpec(request.ParentFolderId), cancellationToken);
+            folder = await folderRepository.FirstOrDefaultAsync(new GetFolderByIdSpec(folder?.ParentFolderId ?? request.ParentFolderId), cancellationToken);
             if (folder.Type == Core.Enums.FolderType.Storage)
                 break;
         } while (folder != null || folder.Type != Core.Enums.FolderType.User || folder.ParentFolder != null || folder.ParentFolder?.Id != Guid.Empty);
