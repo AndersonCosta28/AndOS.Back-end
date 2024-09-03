1042600d39a557e9b8ff5240500b13bbc31a58f2
Anderson da Silva Costa <anderson.scosta28@gmail.com>
2024-09-03 17:12:26 -0300

    add pgadmin

--- AndOS.AppHost/Program.cs
+++ AndOS.AppHost/Program.cs
@@ -3,19 +3,21 @@ using Microsoft.Extensions.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgresdb = builder.AddPostgres("pg", port: 5432)
-                                                          .AddDatabase("postgresdb");
+    .WithDataVolume()
+    .WithPgAdmin()
+    .AddDatabase("postgresdb");
 
 var api = builder.AddProject<Projects.AndOS_API>("andos-api")
-                                            .WithExternalHttpEndpoints()
-                                            .WithReference(postgresdb);
+    .WithExternalHttpEndpoints()
+    .WithReference(postgresdb);
 
 if (builder.Environment.IsDevelopment())
 {
     builder.AddAzureStorage("storage")
             .RunAsEmulator(config => config.WithImageTag("latest")
-                                                                                              .WithBlobPort(10000)
-                                                                                              .WithQueuePort(10001)
-                                                                                              .WithTablePort(10002))
+                .WithBlobPort(10000)
+                .WithQueuePort(10001)
+                .WithTablePort(10002))
             .AddBlobs("blob");
 }
 
